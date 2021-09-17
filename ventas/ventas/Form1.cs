c5220387c68d374d7bedd2eba19f0edf9db87f3d
Daniel Zelaya <Daniel Zelaya@DESKTOP-5OJH6A0>
2021-09-17 16:19:40 -0600

    agregando frase motivadora

--- ventas/ventas/Form1.cs
+++ ventas/ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo");
+            MessageBox.Show("Creé en Dios y todo será posible");
         }
     }
 }
